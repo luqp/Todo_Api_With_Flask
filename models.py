aa01886599fe3fc286970778ae4e3165b70f731d
windyludev <windyludev@outlook.com>
2019-07-09 16:06:53 -0400

    Add the todo model

---
+++ models.py
@@ -0,0 +1,17 @@
+import datetime
+from peewee import *
+
+DATABASE = SqliteDatabase('todoList.sqlite')
+
+class Todo(Model):
+    name = CharField()
+    completed = BooleanField(default=False)
+
+    class Meta:
+        database = DATABASE
+
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Todo], safe=True)
+    DATABASE.close()
